754b2b004083af53fa8e287d9860cd672aba1091
chistian95 <kriskorral59@gmail.com>
2018-11-07 18:29:57 +0100

    Estructura base

---
+++ public/js/juego.js
@@ -0,0 +1,15 @@
+var config = {
+  type: Phaser.AUTO,
+  parent: 'juegoDiv',
+  width: 1280,
+  height: 720,
+  render: {
+    antialias: false
+  },
+  banner: false,
+  backgroundColor: '#1976d2',
+  scene: [
+  ],
+}
+
+var game = new Phaser.Game(config);
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket) {
+  console.log(`Nuevo usuario: ${socket.id}`);
+  
+  socket.on('disconnect', function() {
+    console.log(`Usuario desconectado: ${socket.id}`);
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(8081, function() {
+  console.log(`Escuchando en ${server.address().port}`);
+});
